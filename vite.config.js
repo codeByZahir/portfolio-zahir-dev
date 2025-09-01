c7c938442881bc22b4f48a014781d5a365ff7f3d
codeByZahir <zahir.prk22@gmail.com>
2025-09-01 11:27:31 +0530

    run npm build and add slash end ofthe base name

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/portfolio-zahir-dev",
+  base:"/portfolio-zahir-dev/",
   plugins: [react(),tailwindcss()],
 })
